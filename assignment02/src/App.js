c2e31085b21aa45be86dada144e974bbd0df463b
huynh-alex <alexhuynh.c@gmail.com>
2023-03-28 13:42:00 -0500

    Basic flow for assignment

--- assignment02/src/App.js
+++ assignment02/src/App.js
@@ -1,12 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+import { Products } from "./Products";
+
+export function App() {
+  const [products, setProducts] = useState(Products);
+
   return (
-    <div>
+    <div className="bg-white">
+      <div>
+          <div className="relative mt-2 rounded-md shadow-sm">
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Search"
+          />
+        </div>
+      </div>
 
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+
+
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <div key={product.id} className="group relative">
+              <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                <img
+                  src={product.imageSrc}
+                  alt={product.name}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {product.name}
+                    </a>
+                  </h3>
+                </div>
+                <p className="text-sm font-medium text-gray-900">
+                  {product.price}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ assignment02/src/Products.js
@@ -0,0 +1,8 @@
+export const Products = [
+  {
+    id: 1,
+    name: "Smiley Face",
+    imageSrc: "https://em-content.zobj.net/thumbs/120/apple/354/smiling-face-with-smiling-eyes_1f60a.png",
+    price: "$1",
+  },
+];
