373e8a2f4ec2727ad07e3c0445cb322e401d702b
huynh-alex <alexhuynh.c@gmail.com>
2023-03-29 10:34:51 -0500

    Add shopping cart button

--- assignment02/src/App.js
+++ assignment02/src/App.js
@@ -15,24 +15,38 @@ export function App() {
       setProducts(filtered);
     }
   }
-
-  function addToCart(product) {
-    console.log(product);
+  function removeFromCart(productName) {
+    setCart((prevState) => ({
+      ...prevState,
+      [productName]: Math.max(0, cart[productName] - 1),
+    }));
   }
+  function addToCart(productName) {
+    setCart((prevState) => ({
+      ...prevState,
+      [productName]: cart[productName] + 1,
+    }));
+  }
+  function doneShopping() {}
 
   return (
     <div className="bg-white">
-      <div>
-        <div className="relative mt-2 rounded-md shadow-sm">
-          <input
-            type="text"
-            name="price"
-            id="price"
-            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="Search"
-            onChange={handleSearchChange}
-          />
-        </div>
+      <div className="flex justify-between p-4">
+        <div></div>
+        <input
+          type="text"
+          name="price"
+          id="price"
+          className="block rounded-md border-0 py-2 pl-3 pr-10 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          placeholder="Search"
+          onChange={handleSearchChange}
+        />
+        <button
+          onClick={() => doneShopping()}
+          className="bg-green-500 hover:bg-green-700 py-4 px-4 border-green-700 rounded"
+        >
+          🛒
+        </button>
       </div>
 
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -56,11 +70,17 @@ export function App() {
               </div>
               <br></br>
               <div className="flex items-center justify-center">
-                <button onClick={()=>addToCart(product.name)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                <button
+                  onClick={() => removeFromCart(product.name)}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                >
                   -
                 </button>
-                <span>&emsp; 0 &emsp;</span>
-                <button onClick={()=>addToCart(product.name)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                <span>&emsp; {cart[product.name]} &emsp;</span>
+                <button
+                  onClick={() => addToCart(product.name)}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                >
                   +
                 </button>
               </div>
