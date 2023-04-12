657a21e2fa1d7822aeaec4794d4fc82c6ede29b8
huynh-alex <alexhuynh.c@gmail.com>
2023-04-12 13:36:48 -0500

    Add SQL baseline

---
+++ final/backend/sql/baseline.sql
@@ -0,0 +1,12 @@
+-- Uncomment this line if schema is not created yet
+-- create schema spede;
+
+create table if not exists spede.benchmark (
+    id int NOT NULL unique AUTO_INCREMENT,
+    test1 int,
+    test_date datetime,
+    ram int,
+    cpu_cores int,
+    os varchar(256)
+    PRIMARY KEY (id)
+);
