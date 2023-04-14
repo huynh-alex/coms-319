6556be4b824e65024afdfc27fb92ea321284c52c
huynh-alex <alexhuynh.c@gmail.com>
2023-04-14 10:19:50 -0500

    Update database

--- final/backend/sql/baseline.sql
+++ final/backend/sql/baseline.sql
@@ -7,6 +7,11 @@ create table if not exists spede.benchmarks (
     test_date datetime,
     ram int,
     cpu_cores int,
-    os varchar(256),
+    cpu_arch varchar(32), -- e.g. amd64
+    os varchar(256), -- os name and version
+    engine varchar(32), -- browser engine and version
+    browser varchar(64), -- browser name and version
+    device varchar(64), -- e.g. device model, type, vendor
+    ua varchar(256), -- user agent
     PRIMARY KEY (id)
 );
