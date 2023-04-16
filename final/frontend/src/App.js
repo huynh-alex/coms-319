7ec4a8ffd72944523193cc07800cfe3ae0964f39
Chris01 <chris01@iastate.edu>
2023-04-16 17:06:12 -0500

    Back Button for Dashboard is implemented.

--- final/frontend/src/App.js
+++ final/frontend/src/App.js
@@ -1,67 +1,25 @@
 import "./App.css";
-
+import React, { useState, useEffect } from "react";
 import { Benchmark } from "./pages/Benchmark";
-import { Orders } from "./pages/Orders.js";
-import { Sidebar } from "./Sidebar";
-import { Products } from "./pages/Products.js";
-import { Customers } from "./pages/Customers.js";
-import { Reports } from "./pages/Reports.js";
-import { Integrations } from "./pages/Integrations.js";
-import { CurrentMonth } from "./pages/Current-month.js";
-import { LastQuarter } from "./pages/Last-quarter.js";
-import { Social } from "./pages/Social-engagement.js";
-import { Sale } from "./pages/Year-end-sale.js";
 import { Testing } from "./pages/Testing.js";
 import { GlobalResults } from "./pages/GlobalResults";
 
 function App() {
-
-  let Component
-  switch  (window.location.pathname) {
-    case "/":
-      Component = Benchmark
-    break
-    case "/Orders":
-      Component = Orders
-      break
-    case "/Products":
-      Component = Products
-      break
-    case "/Customers":
-      Component = Customers
-      break
-    case "/Reports":
-      Component = Reports
-      break
-    case "/Integrations":
-      Component = Integrations
-      break
-    case "/Current-month":
-      Component = CurrentMonth
-      break
-    case "/Last-quarter":
-      Component = LastQuarter
-      break
-    case "/Social":
-      Component = Social
-      break
-    case "/Sale":
-      Component = Sale
-      break
-    case "/Testing":
-      Component = Testing
-      break
-    case "/GlobalResults":
-      Component = GlobalResults
-      break
-
-  }
+  const [page, changePage] = useState("Benchmark");
   return (
-    <div className="container-fluid">
-      <div className="row">
-          <Sidebar/>
-          <Component />
-      </div>
+    <div className="h-screen">
+      <Benchmark
+        isActive={page === "Benchmark"}
+        changePage={changePage}
+      />
+      <Testing
+        isActive={page === "Testing"}
+        changePage={changePage}
+      />
+      <GlobalResults
+        isActive={page === "GlobalResults"}
+        changePage={changePage}
+      />
     </div>
   )
 }
--- final/frontend/src/pages/Benchmark.js
+++ final/frontend/src/pages/Benchmark.js
@@ -1,23 +1,41 @@
-export function Benchmark() {
-  return (
-    <>
+import React, { useState, useEffect } from "react";
+
+export function Benchmark({
+  isActive,
+  changePage,
+}) {
+
+  function testRun(){
+    changePage("Testing");
+  }
+  function results(){
+    changePage("GlobalResults");
+  }
+  return !isActive ? (
+    <></>
+  ) : (
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             Main Page
           </div>
           <div>
-          <a class="nav-link" href="/Testing">
-            <button >
-                Run Test
+            <button id="Testing-button"
+            onClick={() => {
+              testRun();
+            }}
+            >
+                Run Benchmark
               </button>
-              </a>
-          <a class="nav-link" href="/GlobalResults">
-            <button >
-                Results!
+                        
+            <button id="results-button"
+            onClick={() => {
+              results();
+            }}>
+                Global Results
               </button>
-              </a>
+              
           </div>
         </main>
-    </>
+    
   );
 }
