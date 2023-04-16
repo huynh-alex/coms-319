06ddab7b0de36f1c46f556ec9c485319fb4cce8b
Chris01 <chris01@iastate.edu>
2023-04-16 13:09:44 -0500

    Basic Page navigation along with NavBar

--- final/frontend/src/App.js
+++ final/frontend/src/App.js
@@ -1,14 +1,56 @@
 import "./App.css";
-import { Benchmark } from "./Benchmark";
+import { Benchmark } from "./pages/Benchmark";
+import { Orders } from "./pages/Orders.js";
 import { Sidebar } from "./Sidebar";
-
+import { Products } from "./pages/Products.js";
+import { Customers } from "./pages/Customers.js";
+import { Reports } from "./pages/Reports.js";
+import { Integrations } from "./pages/Integrations.js";
+import { CurrentMonth } from "./pages/Current-month.js";
+import { LastQuarter } from "./pages/Last-quarter.js";
+import { Social } from "./pages/Social-engagement.js";
+import { Sale } from "./pages/Year-end-sale.js";
 
 function App() {
+  let Component
+  switch  (window.location.pathname) {
+    case "/":
+      Component = Benchmark
+    break
+    case "/Orders":
+      Component = Orders
+      break
+    case "/Products":
+      Component = Products
+      break
+    case "/Customers":
+      Component = Customers
+      break
+    case "/Reports":
+      Component = Reports
+      break
+    case "/Integrations":
+      Component = Integrations
+      break
+    case "/Current-month":
+      Component = CurrentMonth
+      break
+    case "/Last-quarter":
+      Component = LastQuarter
+      break
+    case "/Social":
+      Component = Social
+      break
+    case "/Sale":
+      Component = Sale
+      break
+
+  }
   return (
     <div className="container-fluid">
       <div className="row">
           <Sidebar/>
-          <Benchmark/>
+          <Component />
       </div>
     </div>
   )
--- final/frontend/src/Sidebar.js
+++ final/frontend/src/Sidebar.js
@@ -7,19 +7,19 @@ export function Sidebar() {
       <div class="position-sticky pt-3 sidebar-sticky">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">
+            <a class="nav-link active" aria-current="page" href="/">
               <span data-feather="home" class="align-text-bottom"></span>
               Dashboard
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/Orders">
               <span data-feather="file" class="align-text-bottom"></span>
               Orders
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/Products">
               <span
                 data-feather="shopping-cart"
                 class="align-text-bottom"
@@ -28,19 +28,19 @@ export function Sidebar() {
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/Customers">
               <span data-feather="users" class="align-text-bottom"></span>
               Customers
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/Reports">
               <span data-feather="bar-chart-2" class="align-text-bottom"></span>
               Reports
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/Integrations">
               <span data-feather="layers" class="align-text-bottom"></span>
               Integrations
             </a>
@@ -49,31 +49,31 @@ export function Sidebar() {
 
         <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
           <span>Saved reports</span>
-          <a class="link-secondary" href="#" aria-label="Add a new report">
+          <a class="link-secondary" href="/Saved-reports" aria-label="Add a new report">
             <span data-feather="plus-circle" class="align-text-bottom"></span>
           </a>
         </h6>
         <ul class="nav flex-column mb-2">
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/Current-month">
               <span data-feather="file-text" class="align-text-bottom"></span>
               Current month
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/Last-quarter">
               <span data-feather="file-text" class="align-text-bottom"></span>
               Last quarter
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/Social">
               <span data-feather="file-text" class="align-text-bottom"></span>
               Social engagement
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="/Sale">
               <span data-feather="file-text" class="align-text-bottom"></span>
               Year-end sale
             </a>
