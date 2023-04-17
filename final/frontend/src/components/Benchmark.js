17ae7f8b193bf2e85448b1259199a8d7a0153ab7
Chris01 <chris01@iastate.edu>
2023-04-17 18:05:51 -0500

    Basic Accordion developed using Bootstrap class.

--- final/frontend/src/components/Benchmark.js
+++ final/frontend/src/components/Benchmark.js
@@ -12,16 +12,31 @@ export function Benchmark({ isActive, changePage }) {
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           Benchmark
         </div>
-        <div className="col-span-2 p-4">
+        <div>
             <button id="Testing-button"
             onClick={() => {
               testRun();
             }}
+            class="bg-green-500 hover:bg-green-700 py-4 px-4 border-green-700 rounded"
             >
                 Run Benchmark
               </button>
               
           </div>
+          <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="headingOne">
+               <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  FAQs
+                </button>
+              </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                FAQs about Benchmarks
+              </div>
+            </div>
+          </div>
+  </div>
       </main>
     </>
   );
