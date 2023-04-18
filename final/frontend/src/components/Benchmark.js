c3c9cc7bf8d343e497d30abfbd9e560c3a00c75b
Chris01 <chris01@iastate.edu>
2023-04-17 22:46:43 -0500

    Took out Nested Accordion implementation.

--- final/frontend/src/components/Benchmark.js
+++ final/frontend/src/components/Benchmark.js
@@ -23,16 +23,6 @@ export function Benchmark({ isActive, changePage }) {
               </button>
               
           </div>
-          <div class="accordion" id="accordionExample">
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingOne">
-               <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                  FAQs
-                </button>
-              </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                
                 <div class="accordion" id="sub-accordionExample">
                   <div class="accordion-item">
                     <h2 class="accordion-header" id="sub-headingOne">
@@ -59,10 +49,10 @@ export function Benchmark({ isActive, changePage }) {
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
+              
+            
+          
+        
       </main>
     </>
   );
