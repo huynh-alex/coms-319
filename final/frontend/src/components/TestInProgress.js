c2d88ca7b540c49dd1c8020af4713b1ca3975bae
huynh-alex <alexhuynh.c@gmail.com>
2023-04-20 14:24:06 -0500

    Add table on TestInProgress page

--- final/frontend/src/components/TestInProgress.js
+++ final/frontend/src/components/TestInProgress.js
@@ -3,15 +3,34 @@ import ReactLoading from "react-loading";
 
 export function TestInProgress({ isActive, changePage }) {
   const [benchmarkResults, setBenchmarkResults] = useState({});
-  const [testsCompleted, setTestsCompleted] = useState(0);
+  const [benchmarksCompleted, setBenchmarksCompleted] = useState(0);
 
-  const benchmarkNames = ["Digits of Pi"];
-  const benchmarkLocations = ["./Benchmarks/DigitsOfPi.js"];
+  const benchmarkNames = ["Digits of Pi", "Digits of Pi 2", "Digits of Pi 3"];
+  const benchmarkLocations = [
+    "./Benchmarks/DigitsOfPi.js",
+    "./Benchmarks/DigitsOfPi2.js",
+    "./Benchmarks/DigitsOfPi3.js",
+  ];
 
   useEffect(() => {
     if (isActive) {
+      const tableBody = document.getElementById("table-body");
+
+      for (var i in benchmarkNames) {
+        const newRow = tableBody.insertRow();
+        const newCell1 = newRow.insertCell(0);
+        const newCell2 = newRow.insertCell(1);
+        const newCell3 = newRow.insertCell(2);
+        newCell1.innerHTML = parseInt(i) + 1;
+        newCell2.innerHTML = benchmarkNames[i];
+        newCell3.innerHTML = `<div class="spinner-border spinner-border-sm" role="status" >
+          <span class="sr-only"></span>
+        </div>`;
+        newCell3.setAttribute('id', benchmarkNames[i]);
+      }
       startBenchmarks();
     }
+
   }, [isActive]);
 
   useEffect(() => {
@@ -27,6 +46,8 @@ export function TestInProgress({ isActive, changePage }) {
     var myWorker = new Worker(benchmarkLocation);
     myWorker.postMessage("Start");
 
+    const tableBody = document.getElementById("table-body");
+
     myWorker.onmessage = function (e) {
       var end = performance.now();
       // console.log(`Worker completed with value ${e.data}`);
@@ -40,16 +61,22 @@ export function TestInProgress({ isActive, changePage }) {
         ...benchmarkResults,
         [benchmarkName]: roundedTime,
       }));
-      setTestsCompleted(testsCompleted + 1);
+      setBenchmarksCompleted(benchmarksCompleted + 1);
+
+      const cell = document.getElementById(benchmarkName);
+      cell.innerHTML = roundedTime;
     };
+  }
+  function addTableRow(benchmarkNum, benchmarkName, runtime) {
+    // Modify the content of the new row
   }
 
   useEffect(() => {
-    if (testsCompleted == benchmarkNames.length) {
+    if (benchmarksCompleted == benchmarkNames.length) {
       const saveResultsButton = document.getElementById("saveResultsButton");
       saveResultsButton.disabled = false;
     }
-  }, [testsCompleted]);
+  }, [benchmarksCompleted]);
 
   function startBenchmarks() {
     for (var i in benchmarkNames) {
@@ -69,6 +96,17 @@ export function TestInProgress({ isActive, changePage }) {
         <div className="d-flex justify-content-center align-items-center text-align-center">
           <ReactLoading type="balls" color="#0000FF" height={100} width={200} />
         </div>
+
+        <table style={{ marginTop: "10rem" }} className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Test #</th>
+              <th scope="col">Test Name</th>
+              <th scope="col">Runtime (s)</th>
+            </tr>
+          </thead>
+          <tbody id="table-body"></tbody>
+        </table>
 
         <div style={{ position: "absolute", bottom: "1rem", right: "1rem" }}>
           <button
