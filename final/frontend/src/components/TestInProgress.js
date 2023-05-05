5deb173148bc2423cf3e0754a675eb91c28631b0
huynh-alex <alexhuynh.c@gmail.com>
2023-05-05 10:36:23 -0500

    Add conditional text on TestInProgress.js

--- final/frontend/src/components/TestInProgress.js
+++ final/frontend/src/components/TestInProgress.js
@@ -159,7 +159,8 @@ export function TestInProgress({
     <div>
       <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <strong>Test in Progress</strong>
+        <strong className={`${benchmarksCompleted.count === benchmarkNames.length ? "collapse" : ""}`}>Test in Progress</strong>
+        <strong className={`${benchmarksCompleted.count === benchmarkNames.length ? "" : "collapse"}`}>Test Completed</strong>
         </div>
 
         <div className="d-flex justify-content-center align-items-center text-align-center">
